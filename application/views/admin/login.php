92e6f6f967f0d99565dd0dd34ab898dfe8f16f6b
unknown <>
2016-01-25 22:41:00 +0800

    view of login finished

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -1,21 +1,38 @@
 <html>
 <head>
-<title>My Form</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>my blog</title>
+   <link rel="stylesheet" href="http://apps.bdimg.com/libs/bootstrap/3.3.0/css/bootstrap.min.css">
+   <script src="http://apps.bdimg.com/libs/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+   <script src="http://apps.bdimg.com/libs/jquery/2.1.1/jquery.min.js"></script>
 </head>
 <body>
 <?php echo form_open('admin/login'); ?>
+<form class="form-horizontal">
+	<div class="form-group col-sm-12" style="margin-top:15%">
+	    
+	    <center><label  style="padding-top:5px" class="col-sm-1 col-sm-offset-4 control-label">用户名</label></center>
+	    <div class="col-sm-2">
+	      <input type="text" class="form-control" name="username" value="<?php echo set_value('username'); ?>" placeholder="Username">
+	    </div>
+	    <?php echo form_error('username'); ?>
+	</div>
 
-<?php echo (empty($error_message))?'':'<li>'.$error_message.'</li>'; ?>
-<h5>Username</h5>
-<?php echo form_error('username'); ?>
-<input type="text" name="username" value="<?php echo set_value('username'); ?>" size="50" />
-
-<h5>Password</h5>
-<?php echo form_error('password'); ?>
-<input type="password" name="password" value="<?php echo set_value('password'); ?>" size="50" />
-
-<div><input type="submit" name="submit" value="login" /></div>
-
+	<div class="form-group col-sm-12" style="margin-top:2%">
+		<center>	    <label style="padding-top:5px" class="col-sm-1 col-sm-offset-4 control-label">密码</label>
+</center>
+	    <div class="col-sm-2">
+	      <input type="password" class="form-control" name="password" value="<?php echo set_value('password'); ?>" placeholder="Password">
+	    </div>
+	    <?php echo form_error('password'); ?>
+	</div>
+  <div class="form-group">
+    <div class="col-sm-2 col-sm-offset-6" style="margin-top:1%">
+      <button type="submit" class="btn btn-default">Sign in</button>
+    </div>
+  </div>
 </form>
 
 </body>
--- application/views/admin/success.php
+++ application/views/admin/success.php
@@ -1,12 +1,41 @@
+<!DOCTYPE html>
 <html>
 <head>
-<title>My Form</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>my blog</title>
+   <link rel="stylesheet" href="http://apps.bdimg.com/libs/bootstrap/3.3.0/css/bootstrap.min.css">
+   <script src="http://apps.bdimg.com/libs/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+   <script src="http://apps.bdimg.com/libs/jquery/2.1.1/jquery.min.js"></script>
 </head>
 <body>
-
-<h3>Your form was successfully submitted!</h3>
-
-<p><?php echo anchor('admin/login', 'Try it again!'); ?></p>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-sm-1">
+    </div>
+    <div class="col-sm-10"style="padding-top:2%">
+	  <div class="row">
+        <div class="col-sm-3" style="float:right;margin-top:5px">
+             
+        </div><!-- /.col-lg-6 -->
+          
+      </div>
+      <div class="row">
+      </div>
+     </div>
+      
+    </div>
+    
+  <div class="row">
+    <div class="col-sm-2" style="padding-top:20px">
+    </div>
+    <div class="col-sm-8" style="padding-top:20px">
+     
+    </div>
+  </div>
+</div>
 
 </body>
+
 </html>
