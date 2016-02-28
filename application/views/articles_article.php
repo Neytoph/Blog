30b9158389c03068091fe0032b4dd842c40709e6
Neytoph <keith_0503@163.com>
2016-02-28 16:12:31 +0800

    fix bug

--- application/views/articles_article.php
+++ application/views/articles_article.php
@@ -11,8 +11,8 @@ $html = MarkdownExtra::defaultTransform($article[0]['content']);
 
 
 ?>
-  <div class="col-sm-8 col-sm-offset-2"  style="background-color: #FFF;margin-top:20px;border-radius: 8px;box-shadow:5px 5px 8px #DDDDDD;">
-    <div class="article" style="padding-top:50px;">
+  <div class="col-sm-8 col-sm-offset-2"  style="background-color: #FFF;margin-top:20px;border-radius: 8px;box-shadow:5px 5px 8px #DDDDDD,-5px -5px 8px #DDDDDD;">
+    <div class="article" style="padding-top:50px;padding-bottom:50px;">
       <h2 class="text-center">
         <?php echo $article[0]['title'];?>
       </h2>
