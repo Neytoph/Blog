4e534aec530e00ade6ec286b90895a8caface551
Neytoph <keith_0503@163.com>
2016-03-14 11:19:30 +0800

    little change

--- application/views/tag_show.php
+++ application/views/tag_show.php
@@ -5,7 +5,7 @@
      <li class="active"><button type='button' class='btn btn-<?php echo $data[0]['tag_button_type']?> btn-md'><?php echo $data[0]['tag_name']?></button></li>
     </ol>
         <?php foreach ($data as $key => $value): ?>
-            <blockquote style="background-color: #FFF;margin-top:20px;border-radius: 8px;box-shadow:5px 5px 8px #DDDDDD;"><a href='<?php echo site_url("Articles/article/{$value["id"]}");?>'><h2><?php echo $value['title']?></h2></a><small>Someone famous in <cite title="Source Title">Source Title</cite></small></blockquote>
+            <blockquote style="background-color: #FFF;margin-bottom:-20px;border-radius: 8px;box-shadow:5px 5px 8px #DDDDDD;"><a href='<?php echo site_url("Articles/article/{$value["id"]}");?>'><h2><?php echo $value['title']?></h2></a><small><cite title="Source Title"> published at <?php echo $value['published_at']?></cite></small></blockquote>
           
           <hr style="solid">
         <?php endforeach ?>
