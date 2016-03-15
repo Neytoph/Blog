db00aacfe5f8e88f37ab473fb66822493d8a88f5
unknown <>
2016-03-15 22:56:09 +0800

    add search module

--- application/controllers/Search.php
+++ application/controllers/Search.php
@@ -19,73 +19,43 @@ class Search extends CI_Controller {
 	 */
 	public function show()
 	{
+        $this->load->helper('url');
 		$pattern = $_POST['pattern'];
 		$this->load->database();
 		$this->load->model('articles_model');
-		$data['data'] = $this->articles_model->getAllArticles();
-		var_dump($pattern);
-		foreach ($data['data'] as $key => $value) {
-			// echo $a = unicode_encode($pattern);
-			echo iconv('UTF-8', 'UCS-2', $pattern);
-			// echo htmlspecialchars($pattern, $value['content']);
-			// var_dump($value['content']);
-			preg_match("/{$pattern}/i", $value['content'],$matches);var_dump($matches);
+		$articles_info = $this->articles_model->getAllArticles();
+		foreach ($articles_info as $key => $value) {
+            preg_match_all("/{$pattern}/i", $value['title'],$title_matches);
+			preg_match_all("/{$pattern}/i", $value['content'],$content_matches);
+            if(!empty($title_matches[0]) or !empty($content_matches[0])){
+                $temp_arr['id'] = $value['id'];
+                $temp_arr['title'] = $value['title'];
+                $temp_arr['published_at'] = $value['published_at'];
+                $temp_arr['score'] = count($content_matches[0]) + count($title_matches[0])*5;
+                $show_arr[$temp_arr['score']] = $temp_arr;
+
+            }
+            
 		}
-die;
+
+        //对搜索结果根据打分排序
+        @krsort($show_arr);
+        $data['pattern'] = $pattern;
+        $data['show_arr'] = $show_arr;
 		//当前标题（首页，分类，标签，关于我）
-		$data['cur_title'] = array('','active','','');
+		$data['cur_title'] = array('active','','','');
 
-		$this->load->view('header');
-		$this->load->view('menu',$data);
-		$this->load->view('category_show', $data);
-		$this->load->view('footer');
+        if (!empty($data['show_arr'])) {
+            $this->load->view('header');
+            $this->load->view('menu',$data);
+            $this->load->view('search_show', $data);
+            $this->load->view('footer');
+        }else{
+            $this->load->view('header');
+            $this->load->view('menu',$data);
+            $this->load->view('search_failed', $data);
+            $this->load->view('footer');
+        }
+		
 	}
-	public function unicode_encode($name)
-{
-    $name = iconv('UTF-8', 'UCS-2', $name);
-    $len = strlen($name);
-    $str = '';
-    for ($i = 0; $i < $len - 1; $i = $i + 2)
-    {
-        $c = $name[$i];
-        $c2 = $name[$i + 1];
-        if (ord($c) > 0)
-        {    // 两个字节的文字
-            $str .= '\u'.base_convert(ord($c), 10, 16).base_convert(ord($c2), 10, 16);
-        }
-        else
-        {
-            $str .= $c2;
-        }
-    }
-    return $str;
-}
-
-public function unicode_decode($name)
-{
-    // 转换编码，将Unicode编码转换成可以浏览的utf-8编码
-    $pattern = '/([\w]+)|(\\\u([\w]{4}))/i';
-    preg_match_all($pattern, $name, $matches);
-    if (!empty($matches))
-    {
-        $name = '';
-        for ($j = 0; $j < count($matches[0]); $j++)
-        {
-            $str = $matches[0][$j];
-            if (strpos($str, '\\u') === 0)
-            {
-                $code = base_convert(substr($str, 2, 2), 16, 10);
-                $code2 = base_convert(substr($str, 4), 16, 10);
-                $c = chr($code).chr($code2);
-                $c = iconv('UCS-2', 'UTF-8', $c);
-                $name .= $c;
-            }
-            else
-            {
-                $name .= $str;
-            }
-        }
-    }
-    return $name;
-}
 }
---
+++ application/views/search_failed.php
@@ -0,0 +1,10 @@
+<div class="container">
+<center>
+	<div >
+<h1 class="text-center">
+	没有找到匹配结果！
+</h1>
+</div>
+
+</center>
+</div>
